ac707dcaa029fdac1ffe9eb0ec7b8dda9aa3fdf9
Yovani <yovaninavarro1029@gmail.com>
2025-05-12 01:12:26 -0500

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./pages/list/list.module').then( m => m.ListPageModule)
+  },
+  {
+    path: 'multimedia',
+    loadChildren: () => import('./pages/multimedia/multimedia.module').then( m => m.MultimediaPageModule)
   }
 ];
 
